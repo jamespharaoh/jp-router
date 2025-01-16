4df8e689e2f6bc3355ab2ef48557822f664601c6
James Pharaoh <james@pharaoh.uk>
2025-01-16 10:57:28 +0100

    add links to other services on home page

--- frontend/src/home.rs
+++ frontend/src/home.rs
@@ -6,7 +6,11 @@ pub fn home () -> Html {
 		<Template>
 			<section>
 				<h2>{ "Home" }</h2>
-				<p>{ "TODO" }</p>
+				<h3>{ "Links" }</h3>
+				<li><a href="/bandwidthd/">{ "BandwidthD" }</a></li>
+				<li><a href="/darkstat/">{ "DarkStat" }</a></li>
+				<li><a href="/netdata/">{ "Netdata" }</a></li>
+				<li><a href="https://pihole.arago136.es/">{ "Pi-hole" }</a></li>
 			</section>
 		</Template>
 	}
